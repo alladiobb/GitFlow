df2f2974d79deed7d52b10346f92e54e9cbfb2ce
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-08-10 12:34:34 -0300

    criando PR (PullRequest)

---
+++ 05 - PR(PullRequest).sh
@@ -0,0 +1,18 @@
+#criar um Branch 
+git checkout -b feature/contact
+nano contact.html
+git add .
+git commit -m "add contact"
+
+#subindo a branch no GITHUB
+git push origin feature/contact
+
+#Criar a pull request dentro do github
+#Realizar o Merge  dentro do github
+
+#mudar a branch
+git checkout develop
+#ver se foi feita a alteração
+git pull origin develop
+#Excluir a Branch
+git checkout -D feature/contact
