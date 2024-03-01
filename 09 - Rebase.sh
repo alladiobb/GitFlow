b0dd4d5add99205ac7b25556126399f1e4bf906c
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-03-01 10:07:01 -0300

    Refact: improve Rebase file

--- 09 - Rebase.sh
+++ 09 - Rebase.sh
@@ -1,13 +1,28 @@
 #Como resolver Conflitos
+git pull || git push
+
+# hint: You have divergent branches and need to specify how to reconcile them.
+# hint: You can do so by running one of the following commands sometime before
+# hint: your next pull:
+# hint: 
+# hint:   git config pull.rebase false  # merge
+# hint:   git config pull.rebase true   # rebase
+# hint:   git config pull.ff only       # fast-forward only
+# hint: 
+# hint: You can replace "git config" with "git config --global" to set a default
+# hint: preference for all repositories. You can also pass --rebase, --no-rebase,
+# hint: or --ff-only on the command line to override the configured default per
+# hint: invocation.
 
 git pull --rebase
 
-#hint: Resolve all conflicts manually, mark them as resolved with
-#hint: "git add/rm <conflicted_files>", then run "git rebase --continue".
-#hint: You can instead skip this commit: run "git rebase --skip".
-#hint: To abort and get back to the state before "git rebase", run "git rebase --abort".
+# Se der o erro em baixo
+# hint: Resolve all conflicts manually, mark them as resolved with
+# hint: "git add/rm <conflicted_files>", then run "git rebase --continue".
+# hint: You can instead skip this commit: run "git rebase --skip".
+# hint: To abort and get back to the state before "git rebase", run "git rebase --abort".
 
-#ENTRA DENTRO DO ARQUIVO E MODIFIQUE ELE COMO VOCÊ QUISER
+# ENTRA DENTRO DO ARQUIVO E MODIFIQUE ELE COMO VOCÊ QUISER
 
 git rebase --continue
 
