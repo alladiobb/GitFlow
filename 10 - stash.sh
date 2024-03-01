d2b96972d2ca6e2d268c6b947fd74cb74e6a69fc
Alladio Bonesso <13860253+alladiobb@users.noreply.github.com>
2024-03-01 17:11:53 -0300

    Reafact: improve the explanation

--- 10 - stash.sh
+++ 10 - stash.sh
@@ -1,3 +1,7 @@
-#Caso você comece adicionar um outro arquivo e não fez o PULL antes de começar a trabalhar
+#Caso você comece Adicionar/Editar um outro arquivo e não fez o PULL antes de começar a trabalhar
+#Mas você não quer perde o que estava trabalhando use o comando:
+#STASH Significa Esconder em português
+git stash
 
+#Para listar as alterações
 git stash list
